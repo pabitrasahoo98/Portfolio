7d077abce47ad5055a12a537921274681153840e
Pabitra Sahoo <pabitrachin2@gmail.com>
2024-12-03 21:55:19 +0530

    2 commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
+import Experience from './Components/Experience';
+import About from './Components/About';
+import Home from './Components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       HEllo welcome
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/projects" element={<Projects/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/experience" element={<Experience/>}/>
+      <Route path="/about" element={<About/>}/>
+      </Routes>
+      </BrowserRouter>
   );
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,118 @@
-function Header() {
+import React, { useState } from 'react'
+import {AppBar,Box,Divider,Drawer,IconButton,Toolbar,Typography} from "@mui/material"
+import { Link } from 'react-router-dom'
+import MenuIcon from "@mui/icons-material/Menu"
+import "./HeaderStyle.css";
+import  Logo1 from "../images/logo512.png"
+
+
+
+const Header = () => {
+
+const [mobileOpen,setMobileOpen]=useState(false);
+const handleDrawerToggle=()=>{
+    setMobileOpen(!mobileOpen);
+}
+const drawer=(
+    <Box onClick={handleDrawerToggle} sx={{textAlign:'center'}}>
+        
+         
+                
+                <Box>
+                <Link to={"/"}>
+                <Box>
+                    <img src={Logo1} alt="Portfolio" style={{ height: "60px" }} />
+                </Box>
+                <Typography component={"div"} color={"goldenrod"} variant='h6'  sx={{flexGrow:1,my:1}}>
+                PORTFOLIO
+                </Typography>
+                </Link>
+                </Box>
+                <Divider/>
+                <Typography variant="h6" color={"goldenrod"} sx={{
+      my: 2,
+      fontSize: '20px', // Default font size
+      '@media (max-width: 600px)': { // Media query for smaller screens
+        fontSize: '12px', // Decrease font size for smaller screens
+      },
+    }}>
+    </Typography>
+                <ul className='mobile-navigation'>
+                        <li>
+                            <Link to={"/"}>Home</Link>
+                        </li>
+                        <li>
+                        <Link to={"/about"}>About</Link>
+                            
+                        </li>
+                        <li>
+                            <Link to={"/projects"}>Projects</Link>
+                        </li>
+                        <li>
+                        <Link to={"/experience"}>Experience</Link>
+                        </li>
+                        <li>
+                            <Link to={"/contact"}>Contact Me</Link>
+                        </li>
+                    </ul>
+    </Box>
+)
+
   return (
-    <header className="grid grid-cols-1 lg:grid-cols-2">
-      <div className="p-20 lg:p-32 flex justify-center items-center">
-        <img src="/profile-pic.png" className="shadow-xl rounded-full" />
-      </div>
-      <div className=" py-32 flex justify-center items-center flex-col gap-5">
-        <div className="text-3xl font-bold text-gray-700">Hello I'm</div>
-        <div className="text-6xl font-bold text-gray-900">Muhammad Ali</div>
-        <div className="text-3xl font-bold text-gray-700">Fronten Developer</div>
-        <div className="flex gap-3">
-          <button className="ring-1 p-3 rounded-full px-6 hover:bg-gray-800 hover:text-white transition">Download CV</button>
-          <button className="bg-gray-800 text-white hover:bg-gray-900 p-3 rounded-full px-6 transition">Contact Info</button>
-        </div>
-        <div className="flex gap-2">
-          <img src="/linkedin.png" className="h-10" />
-          <img src="/github.png" className="h-10" />
-        </div>
-      </div>
-    </header>
+    <>
+    <Box>
+        <AppBar component={"nav"} sx={{bgcolor:"#74703F" ,minHeight:"70px"}}>
+            <Toolbar>
+                <IconButton color={'inherit'} aria-label='open drawer' edge="start" sx={{mr:2,display:{sm:"none"},}} onClick={handleDrawerToggle}>
+                    <MenuIcon/>
+                </IconButton>
+                <>
+                <Box>
+                    <img src={Logo1} alt="Portfolio" style={{ height: "50px" }}/>
+                </Box>
+                <Typography component={"div"} color={"goldenrod"} variant='h6'  sx={{flexGrow:1}}>
+                PortFolio
+                </Typography>
+                </>
+                <Box sx={{display:{xs:"none",sm:"block"}}}>
+                    <ul className='navigation-menu'>
+                        <li>
+                            <Link to={"/"}>Home</Link>
+                        </li>
+                        <li>
+                        <Link to={"/about"}>About</Link>
+                        
+                        </li>
+                        <li>
+                        <Link to={"/projects"}>Projects</Link>
+                        </li>
+                        <li>
+                        <Link to={"/experience"}>Experience</Link>
+                        </li>
+                        <li>
+                            <Link to={"/contact"}>Contact Me</Link>
+                        </li>
+                    </ul>
+                </Box>
+            </Toolbar>
+        </AppBar>
+        <Box component="nav">
+            <Drawer variant='temporary'
+             open={mobileOpen} 
+             onClose={handleDrawerToggle}
+              sx={{display:{xs:'block',sm:'none'},"& .MuiDrawer-paper":{boxSizing:'border-box',width:'240px',}}}
+              
+              >
+                {drawer}
+            </Drawer>
+
+        </Box>
+        <Box sx={{p:1}}>
+            <Toolbar/>
+        </Box>
+        </Box>
+    </>
   )
 }
-export default Header;
+
+export default Header
